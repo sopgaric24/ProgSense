6fbdfb9bfcf4948cad35e4f1a3073ad4726c29f5
Tristan <tumbagatristanehron31@gmail.com>
2026-01-24 11:46:12 +0800

    delete: removed test connection to supabase

--- src/App.js
+++ src/App.js
@@ -2,26 +2,9 @@ import { useEffect, useState } from 'react';
 import { supabase } from './supabaseClient.js'
 
 function App() {
-  const [connectionStatus, setConnectionStatus] = useState('Connecting...');
-
-  useEffect(() => {
-    async function checkConnection() {
-      const { data, error } = await supabase.auth.getSession();
-      
-      if (error) {
-        setConnectionStatus('Connection Failed ❌');
-      } else {
-        setConnectionStatus('Connected to Supabase! ✅');
-      }
-    }
-
-    checkConnection();
-  }, []);
-
   return (
     <div className="App">
       <h1>Progsense Prototype</h1>
-      <p>Status: {connectionStatus}</p>
     </div>
   );
 }
