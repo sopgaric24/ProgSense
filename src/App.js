fbb538d317810fa802e28587deb5b20cbf2aa70d
sopgaric24 <sop.garic24@gmail.com>
2026-01-24 10:14:35 +0800

    Feat: initial commit, connected to

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { supabase } from './supabaseClient.js'
 
 function App() {
+  const [connectionStatus, setConnectionStatus] = useState('Connecting...');
+
+  useEffect(() => {
+    async function checkConnection() {
+      const { data, error } = await supabase.auth.getSession();
+      
+      if (error) {
+        setConnectionStatus('Connection Failed ❌');
+      } else {
+        setConnectionStatus('Connected to Supabase! ✅');
+      }
+    }
+
+    checkConnection();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Progsense Prototype</h1>
+      <p>Status: {connectionStatus}</p>
     </div>
   );
 }
---
+++ src/supabaseClient.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
+
+console.log("URL Check:", supabaseUrl);
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
